98eb143c894cc6ea032ccab3477c260af80d20d2
Marin Ivicek <cvicek@gmail.com>
2021-09-24 00:24:32 +0200

    sitnice

--- app.js
+++ app.js
@@ -10,17 +10,20 @@ bodyParser = require('body-parser');
 // var router = express.Router(); 
 
 var app = express();
+
 var apiRoutes = require('./routes/api-routes');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cors());
+
+
 app.use('/api', apiRoutes);
 
 module.exports = app;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-// var mongoDB = 'mongodb://127.0.0.1/test_baza'; 
+// var url = 'mongodb://127.0.0.1/test_baza';  // od prije local
 var url = require('./dburl');
 const CA =  {
   tlsCAFile: `${__dirname}/ca-certificate.crt`,
@@ -34,7 +34,6 @@ router.get('/get-all-cars', function (req, res) {
 });
 
 router.post('/add-car', function (req, res) {
-  console.log(req.body);
   var car_instance = new Car({
     make: req.body.make,
     model: req.body.model,
@@ -45,25 +44,18 @@ router.post('/add-car', function (req, res) {
     model: req.body.model,
     year: req.body.year
   }, function (err, result) {
-    console.log('what happended: ', err, result);
     if (err !== null) {
-      console.log('ERROR1: ', err);
       return next(error);
-      // return handleError(err);
     } else if (result === null) {
       car_instance.save(function (err) {
         if (err) {
-          console.log('ERROR2: ', err);
           return next(error);
-          // return handleError(err);
         } else {
           res.status(200).json({
             msg: 'success'
           })
         }
       });
-      //res.send({'status':'ok'});
-
     } else {
       res.status(200).json({  // možda nesto u stilu "res.status(302).json pa to hendlaangular errorHandler?"
         msg: 'exist'
@@ -73,13 +65,13 @@ router.post('/add-car', function (req, res) {
 });
 
 router.get('/filter', function (req, res) {
-  console.log('FILTER: ', req.query);
   let searchTerm = {};
 
   // ova kobasica vjerojatno može elegantnije
   let make = req.query.make;
   let model = req.query.model;
   let year = req.query.year;
+  
   if (make.length > 0) {
     searchTerm.make = make;
   }
@@ -89,36 +81,27 @@ router.get('/filter', function (req, res) {
   if ((year.length > 0) && (year !== null)) {
     searchTerm.year = year;
   }
-  console.log('search Object: ', searchTerm);
   
   
   Car.find(searchTerm, function (err, data) {  // fuzzySearch  find
-    console.log('car.find start');
     if (err) {
-      console.log('err: ', err);
       return next(err);
     } else {
-      console.log('data: ', data);
       res.json(data);
     }
-    console.log(data);
   });
   })
 
 router.delete('/delete-car/:id', function (req, res) {    // delete umjesto get
-  console.log('req.params.id: ', req.params.id);
   Car.findByIdAndRemove(req.params.id, (error, data) => {
     if (error) {
-      console.log('error: ', error);
       return next(error);
     } else {
-      console.log('found: ', data);
       res.status(200).json({
         msg: data
       })
     }
   })
 });
-
 
 module.exports = router;
